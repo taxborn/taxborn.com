7cf9b9b6e0b8f5a294571c54f2097f66167a8341
Braxton Fair <hello@taxborn.com>
2021-05-10 17:04:46 -0500

    Add tailwindcss to the project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1 class="text-3xl text-indigo-500">This is a test</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>taxborn.com | @yield('title')</title>
+
+        <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
