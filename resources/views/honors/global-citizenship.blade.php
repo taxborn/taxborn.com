71459d7ea7b8007bb29aa9da7ce95d7e938b1844
Braxton Fair <hello@taxborn.com>
2021-05-16 17:46:17 -0500

    Upload honors section

--- resources/views/honors/global-citizenship.blade.php
+++ resources/views/honors/global-citizenship.blade.php
@@ -5,4 +5,18 @@
 @section('content')
         <h1 class="title">Global Citizenship.</h1>
         <blockquote class="blockquote">Upon graduation, honors students will have demonstrated an increased self-awareness of their own and other cultures, knowledge and understanding of cultural perspectives, and awareness of the connections between language and culture in communication.</blockquote>
+
+
+        <div class="bg-gray-200 shadow px-4 py-2 rounded my-4">
+                <a class="text-2xl text-indigo-500 font-bold cursor-pointer" href="https://docs.google.com/document/d/1ZTtAhpCaeQWLnRgLIddVgKZTJv0Y7h3cPW0XcM4ShkY/edit?usp=sharing" rel="noreferrer" target="_blank">
+                    ISHA Yoga Immersion
+                    @include('includes.svg.document')
+                </a>
+            <p class="muted">Summer 2016</p>
+            <p class="subtext my-2">In the summer of 2016, I took a trip to Tennessee with my family and did a week long youth yoga camp with my sister.
+                My mom was very interested in meditation and yoga practice, and decided to take my family on a trip and introduced my family to this practice.
+                On this trip, I went from a culture of a typical middle class mid western family, to being immersed in a community largely centered around
+                Indian and southern Asian culture. The food, the manners, the social interactions, and even the routine was much different than what I was used
+                to, but an experience I could never forget.</p>
+        </div>
 @endsection
