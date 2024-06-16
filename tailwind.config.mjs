8ba04bab124d820bb279047d5a2bce77f724083d
Braxton Fair <hello@taxborn.com>
2024-06-16 18:43:32 -0500

    add overpass font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Overpass Variable', ...defaultTheme.fontFamily.sans],
+			}
+		},
 	},
 	plugins: [],
 }
