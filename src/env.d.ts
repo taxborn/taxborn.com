b68a8b3484d026f2b0e020824c32f9e86d61d2ac
Braxton Fair <hello@taxborn.com>
2024-10-01 16:01:54 -0500

    fix: add types and some light touch ups

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+    theme: {
+      setTheme: (theme: "auto" | "dark" | "light") => void;
+      getTheme: () => "auto" | "dark" | "light";
+      getSystemTheme: () => "light" | "dark";
+      getDefaultTheme: () => "auto" | "dark" | "light";
+    };
+  }
