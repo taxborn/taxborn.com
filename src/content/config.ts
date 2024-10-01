dfbec7cab1890e984d767c6fb83b32702637ea92
Braxton Fair <hello@taxborn.com>
2024-10-01 15:05:13 -0500

    feat: fixup more of the website, prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const noteCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-        title: z.string(),
-        isDraft: z.boolean().default(true),
-        tags: z.array(z.string()),
-        created: z.date()
-    })
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    isDraft: z.boolean().default(true),
+    tags: z.array(z.string()),
+    created: z.date(),
+  }),
 });
 
 export const collections = {
-  'notes': noteCollection,
+  notes: noteCollection,
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
     extend: {
       fontFamily: {
