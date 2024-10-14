6e22fba9652e569cfb50dedc5d73797d1a13a67a
Braxton Fair <hello@taxborn.com>
2024-10-14 18:15:20 -0500

    feat: clean up website styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,38 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import rehypePrettyCode from "rehype-pretty-code";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+import theme from "./catppuccin-color-scheme.json";
+import mdx from "@astrojs/mdx";
+
+const prettyCodeOptions = {
+  theme,
+  onVisitHighlightedLine(node) {
+    node?.properties?.className
+      ? node.properties.className.push("highlighted")
+      : (node.properties.className = ["highlighted"]);
+  },
+  onVisitHighlightedChars(node) {
+    console.log(node);
+    node?.properties?.className
+      ? node.properties.className.push("highlighted-chars")
+      : (node.properties.className = ["highlighted-chars"]);
+  },
+  tokensMap: {},
+};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   output: "hybrid",
+  markdown: {
+    // extendDefaultPlugins: true,
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+    shikiConfig: {
+      theme,
+    },
+  },
 
   adapter: node({
     mode: "standalone",
