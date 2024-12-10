9e4e6885c0a5035b8c8ef58f5d6c91675bc4c629
Braxton Fair <hello@taxborn.com>
2024-12-10 15:31:39 -0600

    feat: add mischief to my about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,18 @@ import expressiveCode from 'astro-expressive-code';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.taxborn.com',
-  integrations: [tailwind(), expressiveCode()],
+  integrations: [tailwind(), expressiveCode({
+    themes: ['catppuccin-mocha', 'dracula', 'andromeeda']
+  })],
   prefetch: {
     prefetchAll: true,
   },
   env: {
     schema: {},
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-mocha'
+    }
   }
 });
