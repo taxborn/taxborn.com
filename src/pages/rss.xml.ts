f75e1fee6bd3d8a1eaccd20eb293515ff69942ca
Braxton Fair <hello@taxborn.com>
2024-12-10 17:08:27 -0600

    feat: add rss feed for notes

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import type { CollectionEntry } from 'astro:content';
+
+export async function GET(context: APIContext) {
+  const notes: CollectionEntry<'notes'>[] = await getCollection('notes');
+
+  return rss({
+    title: "taxborn.com - Braxton's notes",
+    description: 'A collection of organized, and disorganized notes in computer science, math, and my life.',
+    site: context.site ?? 'https://www.taxborn.com',
+    items: notes.map((note) => ({
+      title: note.data.title,
+      description: note.data.description,
+      pubDate: note.data.created_at,
+      link: `/note/${note.id}`
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
