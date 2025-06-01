f6b7aaf5b4952babebcbbc7d8bfffb0782991cd7
Braxton Fair <hello@taxborn.com>
2025-06-01 12:03:20 -0500

    feat: start layout work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Atkinson Hyperlegible",
+        cssVariable: "--font-atkinson-hyperlegible",
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
