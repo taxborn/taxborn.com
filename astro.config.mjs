1f9e93669719b9ff98a0b4707eb2a763f2bd20bd
Braxton Fair <hello@taxborn.com>
2025-06-01 15:17:55 -0500

    chore(astro): some configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.taxborn.com",
+  prefetch: {
+    prefetchAll: true,
+  },
   experimental: {
     fonts: [
       {
