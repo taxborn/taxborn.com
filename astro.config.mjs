09cbef6293453c36531352c4ef96e15454b32deb
Braxton Fair <hello@taxborn.com>
2025-07-12 14:49:50 -0500

    security: enable csp plugin if deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.taxborn.com",
-  // experimental: {
-  //   csp: true,
-  // },
+  experimental: {
+    csp: isProduction,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
