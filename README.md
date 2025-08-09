ba01566c9434ddfdfa86c536a905c4827d49edf6
Braxton Fair <hello@taxborn.com>
2025-08-09 18:39:26 -0500

    Rewrite (#8)
    Reviewed-on: https://forgejo.mischief.town/taxborn/taxborn.com/pulls/8

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # [taxborn.com](https://www.taxborn.com)
-My personal website, and digital garden.
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,24 @@ import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.taxborn.com",
-  trailingSlash: "always",
-  experimental: {
-    csp: isProduction,
-    fonts: [
-      {
-        provider: fontProviders.google(),
-        name: "JetBrains Mono",
-        cssVariable: "--font-jetbrains-mono",
-      },
-      {
-        provider: fontProviders.google(),
-        name: "Atkinson Hyperlegible",
-        cssVariable: "--font-atkinson",
-      },
-    ],
-  },
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "Atkinson Hyperlegible Next",
+        cssVariable: "--font-atkinson-hyperlegible-next",
+      },
+      {
+        provider: fontProviders.fontsource(),
+        name: "Atkinson Hyperlegible Mono",
+        cssVariable: "--font-atkinson-hyperlegible-mono",
+      },
+    ],
   },
 });
