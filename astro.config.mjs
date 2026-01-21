af05bad23e0bc7eb2be039687de239578882f088
Braxton Fair <hello@taxborn.com>
2026-01-20 21:17:32 -0600

    feat: start work on navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   csp: true,
-
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.bunny(),
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
