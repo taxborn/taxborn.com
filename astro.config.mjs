127a497b9d30b134fd662e4f2a374bff20e9849c
Braxton Fair <hello@taxborn.com>
2026-08-10 17:11:41 -0500

    feat: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://www.taxborn.com',
-
   vite: {
     plugins: [tailwindcss()]
   }
